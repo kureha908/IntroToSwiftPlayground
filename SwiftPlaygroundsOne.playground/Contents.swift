7cadfab09e54cdfc52354a4cb9da4de67f213d7e
Kureha <kureha@hawaii.edu>
2019-01-17 14:37:15 -1000

    Final Changes

--- SwiftPlaygroundsOne.playground/Contents.swift
+++ SwiftPlaygroundsOne.playground/Contents.swift
@@ -8,7 +8,7 @@ var str = "Hello, playground"
 //Now we assign variable to a string
 var myFirstVariable = "I've created my first variable"
 //Declare and assign all in one line
-var mySecondVariable = "and look I added two strings together"
+var mySecondVariable = " and look I added two strings together"
 //Let's add two strings together
 var myThirdVariable = myFirstVariable + mySecondVariable
 print(myThirdVariable)
